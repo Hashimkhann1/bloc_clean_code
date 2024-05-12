96e450844ba7a5775f25b5a905e275ee7f763c51
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-05-12 23:11:16 +0500

    home view

---
+++ lib/views/home_view/home_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Home',style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),))
+    );
+  }
+}
