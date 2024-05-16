d35ed86cf3027557b8e498d8b7a9ea718544efb8
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-05-16 22:52:08 +0500

    updated

---
+++ lib/views/login_view/login_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
